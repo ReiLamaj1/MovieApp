7ad0825ea1efe02b3f4d5d5048472d2dbe175016
ReiLamaj1 <154746091+ReiLamaj1@users.noreply.github.com>
2024-08-24 13:55:31 +0200

    Init commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+    colors:{
+      'logocolor':'#000000',
+    },
+  },
+  plugins: [],
+}
